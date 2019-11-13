f3bea4e1019f559b5076e392448a3d0a0d815a67
韦永愿 <weiyongyuan@fe-cred.com>
2019-11-13 15:59:35 +0800

    react跨组件传值

--- react组件传值/src/component/Children.js
+++ react组件传值/src/component/Children.js
@@ -1,24 +1,11 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react';
 export default class Children extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            name: '我是子组件',
-            msg: '子组件传值给父组件'
-        }
-    }
-
-    toParent = () => {
-        // console.log(this.props.parent.getChildrenMsg.bind(this, this.state.msg))
-        this.props.parent.getChildrenMsg(this, this.state.msg)
-    }
-
     render() {
         return (
             <div>
-                <h2>{ this.state.name }</h2>
-                <button onClick={ this.toParent }>子组件传入给父组件</button>
+                这是父组件传值内容：{this.props.parent}
+                <br />
+                <input type="text" onChange={e => { this.props.hanldeChange(e.target.value) }} />
             </div>
         )
     }
--- react组件传值/src/component/Parent.js
+++ react组件传值/src/component/Parent.js
@@ -1,32 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Children from './Children'
 
 export default class Parent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: '我是父组件',
-            msg: '父组件传值给子组件',
-            childrenMsg: ''
+            data: "父组件内容",
+            text: ""
         }
     }
-
-    getChildrenMsg = (result, msg) => {
-        // console.log(result, msg)
-        // 很奇怪这里的result就是子组件那bind的第一个参数this，msg是第二个参数
+    hanldeChange(text) {
         this.setState({
-            childrenMsg: msg
+            data: text
         })
     }
-
     render() {
         return (
             <div>
-                <h2>{ this.state.name }</h2>
-                <h3>子组件传来的值为：{ this.state.childrenMsg }</h3>
-                <h3>我要引入子组件了：</h3>
-                <hr/>
-                <Children parent={ this } />
+                <p>这是子组件传过来的内容：{this.state.text}</p>
+                <Children parent={this.state.data} hanldeChange={this.hanldeChange.bind(this)} />
             </div>
         )
     }
