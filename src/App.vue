7e7ba25fd7838bef8be47ec9219a551139e660db
读心悦 <dxdexin@outlook.com>
2021-10-13 23:56:56 +0800

    vue-router vuex

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
+<!--
+ * @Author: yongyuan253015@gmail.com
+ * @Date: 2021-10-12 00:17:52
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-10-13 23:55:13
+ * @Description: 文件描述
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view class="routerView"> 
+    <transition :name="transitionName">
+      <component :is="Component"/>
+    </transition>
+  </router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive, toRefs } from "vue";
+import { useRoute, useRouter } from "vue-router";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const router = useRouter();
+    const state = reactive();
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+/*
+ * @Author: yongyuan253015@gmail.com
+ * @Date: 2021-10-12 00:17:52
+ * @LastEditors: 
+ * @LastEditTime: 2021-10-13 23:37:44
+ * @Description: vue应用的入口文件
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
+import  router  from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router)
+app.mount('#app')
