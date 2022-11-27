78e26987f273978802d25320301d51acb7749a4e
duxinyues <yongyuan253015@gmail.com>
2022-11-27 17:09:15 +0800

    vue3

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,85 @@
-<!--
- * @Author: yongyuan253015@gmail.com
- * @Date: 2021-10-12 00:17:52
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-10-13 23:55:13
- * @Description: 文件描述
--->
-<template>
-  <router-view class="routerView"> 
-    <transition :name="transitionName">
-      <component :is="Component"/>
-    </transition>
-  </router-view>
-</template>
-
-<script>
-import { reactive, toRefs } from "vue";
-import { useRoute, useRouter } from "vue-router";
-export default {
-  setup() {
-    const router = useRouter();
-    const state = reactive();
-  },
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style></style>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
