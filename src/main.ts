7381c7f7691c3d7b5b5fb1eb43b82dc39e162078
duxinyues <yongyuan253015@gmail.com>
2022-12-28 23:17:16 +0800

    响应数据

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import ElementPlus from "element-plus";
 import App from './App.vue'
 import './assets/main.css'
 const app = createApp(App)
 app.provide("data","duxin")
 app.mount('#app')
+app.use(ElementPlus)
